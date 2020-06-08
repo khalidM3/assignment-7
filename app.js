b75ce5857ce38414a21df814b0d959276cb0554f
Khalid Mohamud <kmohamud@zipwhip.com>
2020-06-07 18:01:43 -0700

    writing comments

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 'use strict';
 
 function handleSubmit() {
+  // prevent the page from reloading when you click submit
   event.preventDefault();
-  console.log('handleSubmit')
+  // getting values of the inputs 
   var miles = document.getElementById("miles").value;
   var gallons = document.getElementById("gallons").value;
+  // calculating miles per gallon
   var milesPerGallon = miles / gallons;
+  // output result of calculation to mpg field
   document.getElementById("mpg").value = milesPerGallon;
-  console.log(miles, gallons)
 }
